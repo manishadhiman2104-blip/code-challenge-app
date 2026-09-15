f445e5bd2ce9c7328d9a27c76ad29bfff34dcde7
Manisha Dhiman <manishadhiman2104@gmail.com>
2026-09-15 15:03:20 +0530

    Updated quiz application

--- src/data/question.js
+++ src/data/question.js
@@ -150,25 +150,22 @@
   // ---------- Arrays (10) ----------
   "Two Sum Problem": `
 
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] res = twoSum(nums, target);
-        System.out.println("Indices: [" + res[0] + ", " + res[1] + "]");
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
         }
-        return new int[]{-1, -1}; // No match found
+        
+        map.put(nums[i], i);
     }
+    
+    return new int[] {};
 }
+
 
 `
 ,
@@ -1536,7 +1533,7 @@ public class PartitionEqualSubset {
         for (int n: nums){
             for (int t = target; t >= n; t--) if (dp[t-n]) dp[t] = true;
         }
-        return dp[target];
+        return dp[target];co
     }
 }
 `
